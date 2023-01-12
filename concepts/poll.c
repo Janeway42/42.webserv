57cf95fb214596dd84367d84624ac27520a33f46
Corina Popa <cpopa@f1r2s7.codam.nl>
2023-01-12 17:27:35 +0100

    concepts

---
+++ concepts/poll.c
@@ -0,0 +1,37 @@
+#include <poll.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void)
+{
+	struct pollfd mypoll;
+	char name[250];
+	int count = 0;
+
+	//init poll
+	memset(&mypoll, 0, sizeof(mypoll));
+	mypoll.fd = 0;
+	mypoll.events = POLLIN;
+
+	printf("Type name \n");
+
+	while (1)
+	{
+		poll(&mypoll, 1, 100); 
+		// poll(&mypoll, 1, -1) does not block anymore
+		if (mypoll.revents & POLLIN)
+		{
+			read(0, name, sizeof(name));
+			printf("Hi, %s\n", name);
+			break ;
+		}
+		else
+			count++;
+	}
+
+	printf("Count is %d\n", count);
+
+	return (0);
+
+}
