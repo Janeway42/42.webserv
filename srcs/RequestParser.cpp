edfeeeebfa156994919524f04f96005cd33aa94b
JMB <jmurovec@f1r1s4.codam.nl>
2023-02-08 18:49:33 +0100

    Modified appendToRequest. First version with kque works fine with browser and curl. The second version of kque seems to call only once the function appendTorequest, should call it again and until all is read.

--- srcs/RequestParser.cpp
+++ srcs/RequestParser.cpp
@@ -4,6 +4,15 @@
 //
 
 
+
+// c++ kqueue.cpp Server.cpp srcs/Parser.cpp srcs/RequestData.cpp srcs/RequestParser.cpp && ./a.out
+
+// curl -X POST localhost:8080  -H "Content-Type: text/html" -d '{"Id": 79, "status": 3}'
+// curl -X POST localhost:8080  -H "Content-Type: text/html" -d 'abc'
+// curl -X POST localhost:8080  -H "Content-Length: 444"  -H "Content-Type: text/html" -d 'abc'
+
+#include <unistd.h> // sleep
+
 #include "../includes/RequestParser.hpp"
 
 namespace data {
@@ -12,6 +21,9 @@ namespace data {
 Request::Request() {
 	_data = RequestData();
 	_headerDone = false;
+
+	_doneParsing = false;
+	_errorRequest = false;
 }
 
 /** Destructor */
@@ -143,76 +155,127 @@ int Request::storeWordsFromOtherLine(std::string otherLine) {
 				iter++;
 				_data.setRequestContentType(*iter);
 			}
-			else
-				std::cout << RED << "Error: This method is not recognized\n" << RES;
+			// else
+			// 	std::cout << RED << "Error: This method is not recognized - other line\n" << RES;
 		}
 	}
 	return (0);
 }
 
+/*	- What if method is GET (normaly without body) AND content-length is not zero ???
+	- What if method POST and content-length is zero ???
+*/
 
-
-void Request::appendToRequest(const char *str) {
+void    Request::appendToRequest(const char *str) {
 	std::string 			chunk = std::string(str);
-	std::string::size_type 	it;
+	// std::string				strToFind = "image/avif";
+	std::string				strToFind = "\r\n\r\n";
+	//std::string				strToFind = "xxx";
+	std::string::size_type	it;
 
-	// std::cout << "ATR A)\n";
+	std::cout << PUR "AppendToRequest()\n" RES; // sleep(1);
+
 	if (_headerDone == false) {
-		if (chunk.find("\r\n\r\n") == -1) {		// Sep. line not found
-			_header.append(chunk);
-		}
-		else if ((it = chunk.find("\r\n\r\n")) != std::string::npos) { // Found, but has more chars after it
-			_temp.append(chunk);
-			_header.append(chunk.substr(0, it));
-			_headerDone = true;
-			parseHeader(_header);
-			appendLastChunkToBody(it);
+		std::cout << PUR "     _headerDone == FALSE\n" RES;
+		
+		_temp.append(chunk);
+		std::cout << PUR "     chunk appended to _temp\n" RES;	// sleep(1);
+		std::cout << "         chunk: [" BLU << chunk << RES "]\n";	// sleep(1);
+		std::cout << "         _temp: [" BLU << _temp << RES "]\n";	// sleep(1);
 
-		} else {								// found, and it's the end of string
-			it = _temp.find("\r\n\r\n");
-			_header = _temp.substr(0, it);
-			appendToBody(chunk);
+		if ((it = _temp.find(strToFind)) != std::string::npos) {
+			std::cout << PUR "     a)  Found header ending /r/n, maybe there is body\n" RES;	// sleep(1);
+			_header.append(_temp.substr(0, it));
 			_headerDone = true;
+			std::cout << "HEADER: [" BLU << _header << RES "]\n";	// sleep(1);
+			parseHeader(_header);
+			// What shall have preference, Content-Length or the actual body ???
+
+			appendLastChunkToBody(it + strToFind.length());
+			std::cout << "BODY:   [" BLU << _body   << RES "]\n\n";	// sleep(1);
+		}
+
+		else if ((it = _temp.find(strToFind)) == std::string::npos ) { // Keep appending, till /r/n id found
+			std::cout << PUR "     b)  Not found /r/n yet. Append _temp_ to _header." RES;
+			std::cout << PUR " (it: " << it << ")\n" RES;	// sleep(1);
+			std::cout << "     _temp: [" BLU << _temp << RES "]\n";	// sleep(1);
 		}
 	}
-	else if (_headerDone == true)
-		appendToBody(chunk);
+	else if (_headerDone == true) {
+			std::cout << PUR "     _headerDone == TRUE\n" RES;	// sleep(1);
+			appendToBody(chunk);
+	}
+	std::cout << PUR "End of AppendToRequest()\n" RES; // sleep(1);
 }
+
+
 
 
 // Last chunk means, last chunk of header section, so first chunk of body
 int Request::appendLastChunkToBody(std::string::size_type it) {
-	_body = _temp.substr(it + 4);
+	_body = _temp.substr(it);
 	
 	// Compare body lenght
 	if (_body.length() > _data.getRequestContentLength()) {
 		std::cout << RED "Error: Body-Length is bigger than expected Content-Length\n" RES;
+		_errorRequest = true;
 		return (1);
 	}
-	// time out etc etc etc
-	// set done 
-	// st errors
+
+	if (_body.length() == _data.getRequestContentLength()) {
+		if (_body.length() == 0 && _data.getRequestContentLength() == 0) {		// Compare body lenght
+			std::cout << GRE "OK (there is no body)\n" RES;
+			_doneParsing = true;
+			return (0);
+		}
+		std::cout << GRE "OK: Body-Length is as expected Content-Length\n" RES;
+		_doneParsing = true;
+		return (0);
+	}
+	// Timeout ???
+	// 		In case of wrong header, body length bigger than body, but body is already done, 
+
 	return (0);
 }
 
 
+
 int Request::appendToBody(std::string req) {
+	std::cout << GRE "AppendToBody()\n" RES;
+	//exit (0);
+
 	_body.append(req);
 	
 	if (_body.length() > _data.getRequestContentLength()) {		// Compare body lenght
-		std::cout << RED "Error: Body-Length is bigger than expected Content-Length\n" RES;
+		std::cout << RED "Error: Body-Length is bigger than expected Content-Length\n" RES; // sleep(2);
+		_errorRequest = true;
 		return (1);
 	}
+	else if (_body.length() == _data.getRequestContentLength()) {		// Compare body lenght
+		std::cout << GRE "OK: Done parsing.\n" RES; // sleep(2);
+		_doneParsing = true;
+		std::cout << "HEADER: [" BLU << _header << RES "]\n";	// sleep(1);
+		std::cout << "BODY:   [" BLU << _body   << RES "]\n\n";	// sleep(1);
+		return (0);
+	}
+	// Maybe not needed
+	else if (_body.length() == 0 && _data.getRequestContentLength() == 0) {		// Compare body lenght
+		std::cout << GRE "There is no body\n" RES;
+		_doneParsing = true;
+		return (0);
+	}
 	// time out etc etc etc
-	// set done 
-	// st errors
-	// std::cout << YEL "ATB, bodyLenght " << _body.length() << "]\n" RES;
 	return (0);
 }
 
 
 int Request::checkStoredVars() {
 	if (_data.getRequestContentLength() == _body.length()) {
+		// if (_body.length() == 0 && _data.getRequestContentLength() == 0) {		// Compare body lenght
+		// 	std::cout << GRE "OK (there is no body)\n" RES;
+		// 	_doneParsing = true;
+		// 	return (0);
+		// }
 		std::cout << GRE "OK: Body size == Content lenght\n" RES;
 	}
 	if (_data.getRequestContentLength() > _body.length()) {
@@ -243,4 +306,31 @@ void Request::printStoredRequestData(data::Request &request) {
 	std::cout << "REQUEST BODY:\n[" PUR << request.getRequestBody() << RES "]\n";
 }
 
+
+bool Request::getDone()
+{
+	return (_doneParsing);
+}
+
+void Request::setDone(bool val)
+{
+	_doneParsing = val;
+}
+
+bool Request::getError()
+{
+	return (_errorRequest);
+}
+
+void Request::setError(bool val)
+{
+	_errorRequest = val;
+}
+
+std::string Request::getTemp()
+{
+	return(_temp);
+}
+
 } // data
+
