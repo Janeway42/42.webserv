a396cc46d1a839096463d8eadb0eebb3e6d71b56
JMB <jmurovec@f1r1s4.codam.nl>
2023-03-30 13:11:48 +0200

    Improved tester (in tests/test_urls.sh)

--- tests/test_urls.sh
+++ tests/test_urls.sh
@@ -1,4 +1,7 @@
-
+RED='\033[31m'
+GRE='\033[32m'
+YEL='\033[33m'
+RES='\033[0m'
 
 cd ../
 rm -rf tests/CurlOut
@@ -19,14 +22,25 @@ function testURLpath {
     curl -i -s -X GET $testPath > $folderCurlOut/$testName &
     sleep 0.1
     if diff $folderCurlOut/$testName $folderCurlCompare/$testName >/dev/null ; then
-        echo "The files are identical"
+        echo -en "${GRE}[OK]${RES}"
+        printf "%-50s" " $testPath"
+        printf "%-50s\n" "The files are identical"
     else
-        echo "The files differ"
+        echo -en "${RED}[OK]${RES}"
+        printf "%-50s" " $testPath"
+        printf "%-50s\n" "The files differ"
     fi
 }
 
 #               URL-PATH                             OUTPUT FILENAME
-testURLpath "localhost:8080"                        "localhost:8080"
-testURLpath "localhost:8080/index_just_text.html"   "localhost:index_just_text.html"
-testURLpath "localhost:8080/index.html"             "localhost:index.html"
-testURLpath "localhost:8080/cgi/index.html"         "localhost:cgi/index.html"
+testURLpath "localhost:8080"                            "localhost:8080"
+testURLpath "localhost:8080/index.html"                 "localhost:index.html"
+testURLpath "localhost:8080/index_just_text.html"       "localhost:index_just_text.html"
+testURLpath "localhost:8080/index_just_text_bible.html" "localhost:index_just_text_bible.html"
+testURLpath "localhost:8080/cgi/cgi_index.html"         "localhost:cgi:cgi_index.html"
+
+### images
+testURLpath "localhost:8080/img_36kb.jpg"               "localhost:img_36kb.jpg"
+
+
+pkill webserv
